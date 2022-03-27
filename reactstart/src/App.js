28487bc7b370eda7e976fcbfded930ce05fd01d6
zhak1230 <zhak1230@gmail.com>
2022-03-27 20:14:10 +0900

    React: #6.1 useEffect

--- reactstart/src/App.js
+++ reactstart/src/App.js
@@ -1,13 +1,38 @@
-import Button from './Button';
-import styles from './App.module.css';
+// import Button from './Button';
+// import styles from './App.module.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => {
+    setValue((prev) => prev + 1);
+  };
+  console.log('i run all the time.');
+
+  useEffect(() => {
+    console.log('CALL THE API...');
+  }, []);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={'continue'} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
 
 export default App;
+
+// useEffect
+// - 두 개의 argument를 가지는 함수
+// - 첫 번째 argument는 우리가 딱 한번만 실행하고 싶은 코드
+// - 두 번째는 [] 배열을 넣어줌
+// -> useEffect가 컴포넌트의 첫 번째 렌더 시점에 iRunOnlyOnce 함수 호출
+// 그리고 상태를 변화시키면 iRunOnlyOnce는 호출되지 않음
+// 즉, 한번만 렌더링 됨
+// 단순화 하여 useEffect(() => {
+// console.log("CALL THE API")
+// },[]); 써도 됨
+
+// // useEffect(callback, [])
+// : 대표적인 사용법 => API를 딱 한번만 호출하고 그 뒤로는 다시는 호출하기 싫은 경우
