d0587e8ec47cee7863418548f4d1c833cb0d1e98
zhak1230 <zhak1230@gmail.com>
2022-03-27 20:51:05 +0900

    React: #6.2 Deps

--- reactstart/src/App.js
+++ reactstart/src/App.js
@@ -4,17 +4,33 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState('');
   const onClick = () => {
     setValue((prev) => prev + 1);
   };
-  console.log('i run all the time.');
+  const onChange = (event) => setKeyword(event.target.value);
 
   useEffect(() => {
-    console.log('CALL THE API...');
+    console.log('I run only once.');
   }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'keyword' & 'counter' changes.");
+  }, [keyword, counter]);
 
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type='text'
+        placeholder='Search here...'
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
@@ -23,16 +39,20 @@ function App() {
 
 export default App;
 
-// useEffect
-// - 두 개의 argument를 가지는 함수
-// - 첫 번째 argument는 우리가 딱 한번만 실행하고 싶은 코드
-// - 두 번째는 [] 배열을 넣어줌
-// -> useEffect가 컴포넌트의 첫 번째 렌더 시점에 iRunOnlyOnce 함수 호출
-// 그리고 상태를 변화시키면 iRunOnlyOnce는 호출되지 않음
-// 즉, 한번만 렌더링 됨
-// 단순화 하여 useEffect(() => {
-// console.log("CALL THE API")
-// },[]); 써도 됨
+// useEffect 글자를 타이핑할 때마다 API를 새로 호출하지 않고 한번만 호출해준다.
 
-// // useEffect(callback, [])
-// : 대표적인 사용법 => API를 딱 한번만 호출하고 그 뒤로는 다시는 호출하기 싫은 경우
+// search keyword에 변화가 있을 때 만! marvel영화를 검색하고 싶을 때
+// 즉, 특정한 코드만 변화했을 때 원하는 코드들을 실행할 수 있는 방법
+// -> useEffect(() => {
+// console.log("SEARCH FOR", keyword);
+// }, []);
+// 이렇게 실행하면 1번만 됨
+// 그리고 []자리에 keyword 써줌
+// -> keyword가 변화할 때 코드를 실행할 거라고 react.js에게 알려주는 것.
+// 이것이 바로 빈 array를 써주었을 때 코드가 단 한번만 실행되는 이유임
+// react가 지켜볼 게 아무것도 없으니 처음 한번만 실행되는 것
+
+// useEffect(() => {
+// console.log("I run when 'keyword & counter' changes.")
+// }, [keyword, counter]);
+// -> 2개도 가능
