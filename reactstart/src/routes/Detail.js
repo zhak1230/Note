b3d5ab2b5c604794c8052ec6b53863a35f22d89d
zhak1230 <zhak1230@gmail.com>
2022-03-31 02:08:25 +0900

    React: #7.7 Publishing

--- reactstart/src/routes/Detail.js
+++ reactstart/src/routes/Detail.js
@@ -1,18 +1,52 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function Detail() {
+  const [movie, setMovie] = useState({});
+  const [loading, setloading] = useState(false);
   const { id } = useParams();
-  const getMovie = async () => {
+  const getMovie = useCallback(async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
-  };
+    setMovie(json.data.movie);
+    setloading(true);
+    console.log(json.data.movie);
+  }, [id]);
+
   useEffect(() => {
     getMovie();
-  }, []);
-  return <h1>Detail</h1>;
+  }, [getMovie]);
+
+  return (
+    <div>
+      {loading ? (
+        <div>
+          <img src={movie.background_image} alt={movie.title} />
+          <div>
+            <div>
+              <img src={movie.medium_cover_image} alt={movie.title} />
+              <div>
+                <h1>{movie.title}</h1>
+                <div>
+                  <h3>{`rating: ${movie.rating}`}</h3>
+                  <h3>{`like:${movie.like_count}`}</h3>
+                </div>
+              </div>
+            </div>
+            <p>{movie.description_intro}</p>
+            <ul>
+              {movie.genres.map((genre, index) => (
+                <li key={index}>{genre}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
