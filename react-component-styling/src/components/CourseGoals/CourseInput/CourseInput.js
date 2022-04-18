d50338f8abee256db2570eb977298ce25268d3f0
zhak1230 <zhak1230@gmail.com>
2022-04-18 23:18:48 +0900

    리액트 컴포넌트 스타일링 마무리

--- react-component-styling/src/components/CourseGoals/CourseInput/CourseInput.js
+++ react-component-styling/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,44 +1,7 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
-
-const FormControl = styled.div`
-   {
-    margin: 0.5rem 0;
-  }
-
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-  }
-
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid #ccc;
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
-
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-
-  &.invalid input {
-    background: #ffd7d7;
-    border-color: red;
-  }
-
-  &.invalid label {
-    color: red;
-  }
-`;
+import styles from './CourseInput.module.css';
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -62,13 +25,54 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl className={!isValid && 'invalid'}>
+      <div
+        className={`${styles['form-control']} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input type='text' onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type='submit'>Add Goal</Button>
     </form>
   );
 };
 
 export default CourseInput;
+
+// import styled from 'styled-components';
+
+// const FormControl = styled.div`
+
+//    {
+//     margin: 0.5rem 0;
+//   }
+
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//   }
+
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid #ccc;
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+
+//   &.invalid input {
+//     background: #ffd7d7;
+//     border-color: red;
+//   }
+
+//   &.invalid label {
+//     color: red;
+//   }
+// `;
