b288102f1420b2418fdadbfc5828f0bff51d4235
zhak1230 <zhak1230@gmail.com>
2022-04-21 16:21:48 +0900

    AddUser 컴포넌트 추가

---
+++ reactpracticeproject/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import AddUser from './components/User/AddUser';
+
+function App() {
+  return (
+    <div>
+      <AddUser />
+    </div>
+  );
+}
+
+export default App;
