34dff16c98f8f620fda5f113d37f1199f4b14bdd
zhak1230 <zhak1230@gmail.com>
2022-04-21 16:36:07 +0900

    재사용 가능한 Card 컴포넌트 생성

--- reactpracticeproject/src/components/User/AddUser.js
+++ reactpracticeproject/src/components/User/AddUser.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+
+import Card from '../UI/Card';
 import styles from './AddUser.module.css';
 
 const AddUser = (props) => {
@@ -7,13 +9,15 @@ const AddUser = (props) => {
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor='username'>Username</label>
-      <input id='username' type='text' />
-      <label htmlFor='age'>Age (Years)</label>
-      <input id='age' type='number' />
-      <button type='submit'>Add User</button>
-    </form>
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor='username'>Username</label>
+        <input id='username' type='text' />
+        <label htmlFor='age'>Age (Years)</label>
+        <input id='age' type='number' />
+        <button type='submit'>Add User</button>
+      </form>
+    </Card>
   );
 };
 
