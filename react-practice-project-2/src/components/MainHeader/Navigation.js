8d30ede4b12f878d4887602be6a1a337a42f9b77
zhak1230 <zhak1230@gmail.com>
2022-04-27 21:22:52 +0900

    useContext 훅으로 컨텍스트에 tapping 하기 (훨신 간단함)

--- react-practice-project-2/src/components/MainHeader/Navigation.js
+++ react-practice-project-2/src/components/MainHeader/Navigation.js
@@ -1,35 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import classes from './Navigation.module.css';
 import AutoContext from '../store/auth-context';
 
 const Navigation = (props) => {
+  const ctx = useContext(AutoContext);
   return (
-    <AutoContext.Consumer>
-      {(ctx) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href='/'>Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href='/'>Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AutoContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href='/'>Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href='/'>Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
